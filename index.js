7e32175518e01cadcf61e6c7114d1dc9557710c5
vinnie <githinjigicheru146@gmail.com>
2024-01-18 09:48:00 +0300

    Test passed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+// const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+// const mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
+// const companyCeo = 'Susan Smith';
